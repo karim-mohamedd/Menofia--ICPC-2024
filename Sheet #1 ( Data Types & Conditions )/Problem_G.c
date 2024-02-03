166b8fe43c83f1ad0d431651dfd743d1381659bd
Karim Mohamed <karimelwaraky50@gmail.com>
2024-02-03 22:17:21 +0200

    Seventh Problem

---
+++ Sheet #1 ( Data Types & Conditions )/Problem_G.c
@@ -0,0 +1,25 @@
+/********************************************************************** Problem G **************************************************************************
+
+Author: Karim Mohamed
+Date: 3 Feb 2024 , 9:42 Pm
+
+Problem Statement
+
+This contest, AtCoder Beginner Contest, is abbreviated as ABC.
+
+When we refer to a specific round of ABC, a three-digit number is appended after ABC. For example, ABC680 is the 680th round of ABC.
+
+What is the abbreviation for the 
+
+N-th round of ABC? Write a program to output the answer.
+***********************************************************************************************************************************************************/
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    
+    printf("ABC");
+    printf("%i",N);
+	return 0;
+}
